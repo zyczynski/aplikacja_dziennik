54c5f8b77e62623594f77abd49556729e3b678f8
bwl <zyczynski7@gmail.com>
2019-06-02 18:19:42 +0200

    dzisiejsze zmiany

--- app/tests/UczenTest.php
+++ app/tests/UczenTest.php
@@ -5,13 +5,15 @@ use PHPUnit\Framework\TestCase;
 class UczenTest extends \PHPUnit\Framework\TestCase
 
 {
+    /**
+     * @test
+     */
     public function testImienia()
     {
         $uczen = new \Dziennik\Uczen\Uczen();
 
 
         $this->assertEquals($uczen->getImie(), 'Bartek');
-
 
     }
 }
